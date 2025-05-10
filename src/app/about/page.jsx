528d943c7e8786f098e351dbd216aa9c958f70fd
Mark Chovava <markchovava@gmail.com>
2025-05-10 16:19:39 +0200

    Working on imge

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,7 +9,7 @@ export default function page() {
     {/* TITLE */}
     <section 
       className="w-[100%] h-[15rem] relative bg-fixed bg-cover text-white bg-blue-800" 
-      style={{backgroundImage: `url('/assets/img/02.png')`}}>
+      style={{backgroundImage: `url('/assets/img/01.jpg')`}}>
         <div className='absolute w-[100%] h-[100%] z-10 bg-black opacity-10'></div>
         <div className='absolute w-full z-20 mx-auto h-[100%] flex flex-col items-center justify-center'>
             <h2 className="text-center  text-[2rem] font-extrabold mb-4">WHO WE ARE?</h2>
