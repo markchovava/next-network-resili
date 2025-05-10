0360bc8544188589489ffb571689bfafe6f81f92
Mark Chovava <markchovava@gmail.com>
2025-05-10 17:17:37 +0200

    Slider corrections

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Link from "next/link";
 import MainSlider from "./_components/MainSlider";
 import CarouselService from "./_components/CarouselService";
 import CarouselPartner from "./_components/CarouselPartner";
+import SectionContact from "./_components/SectionContact";
 
 export default function Home() {
   return (
     <>
     <MainSlider />
+
 
     <section className="w-full py-[5rem]">
       <div className="w-[70%] mx-auto flex flex-col items-center justify-center">
@@ -27,6 +29,7 @@ export default function Home() {
         </Link>
       </div>
     </section>
+
 
     <section className="w-full py-[5rem] text-gray-50 bg-gradient-to-br from-blue-800 to-blue-950">
       <div className="w-[92%] mx-auto flex flex-col items-center justify-center ">
@@ -51,30 +54,8 @@ export default function Home() {
       </div>
     </section>
 
-    <section 
-      className="w-[100%] h-[35rem] lg:h-[25rem] relative bg-fixed bg-cover bg-green-800" 
-      style={{backgroundImage: `url('/assets/img/02.png')`,}}>
-        <div className="absolute z-10 w-[100%] h-[100%] bg-black opacity-30">
-        </div>
-        <div className="py-[5rem] absolute z-20 w-[92%] text-white mx-auto flex flex-col items-center justify-center ">
-            <h2 className="text-center text-[2rem] font-extrabold uppercase mb-4">
-              Engage Us For Free Consultation.
-            </h2>
-            <hr className="w-[10rem] border-white border-b border-[0.4rem]" />
-
-            <p className="py-6 text-center text-[1.4rem] w-[60%]">
-              Our business is to accelerate yours by equiping you with all you need to run smarter.
-            </p>
-
-            <Link href="#">
-              <button className="uppercase px-12 py-4 text-lg rounded-2xl bg-gradient-to-br from-green-500 to-blue-900 text-white ease-in-out duration-300 transition-all hover:drop-shadow-lg hover:scale-110 hover:bg-gradient-to-br hover:from-blue-500 hover:to-blue-950">
-                Count Us Now
-              </button>
-            </Link>
-        </div>
-    </section>
-
-
+    
+    <SectionContact />
 
 
     </>
