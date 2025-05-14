170856ee3653bda647f7da5616d10089ccb5bbfb
Mark Chovava <markchovava@gmail.com>
2025-05-14 11:00:29 +0200

    Profile

--- src/app/admin/profile/page.jsx
+++ src/app/admin/profile/page.jsx
@@ -1,9 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import { FaAngleRight } from 'react-icons/fa'
-import CategoryList from './_components/CategoryList'
-
-
+import ProfileView from './_components/ProfileView'
 
 export default function page() {
   return (
@@ -11,17 +9,15 @@ export default function page() {
     {/* BREADCRUMBS */}
     <section className='w-full border-y border-gray-200 py-1'>
       <ul className='mx-auto w-[92%] flex items-center justify-start gap-2 text-sm'>
-        <Link href='/'>
-        <li>Home</li>
-        </Link>
+        <Link href='/'> <li>Home</li> </Link>
         <li><FaAngleRight /></li>
-        <Link href='/admin'>
-        <li>Admin</li>
-        </Link>
+        <Link href='/admin'> <li>Admin</li> </Link>
+        <li><FaAngleRight /></li>
+        <Link href='/admin/profile'><li className='font-semibold'>Profile</li> </Link>
       </ul>
     </section>
 
-    <CategoryList />
+    <ProfileView />
     </>
   )
 }
